6ad2c1c7af1526dd66b378014ccb83807f738f75
Ruo Liu <ruoliu.dev@gmail.com>
2025-01-07 14:41:22 -0600

    change content

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,6 +1,6 @@
 export const siteConfig = {
     domain: "https://ruoliu2.github.io/",
     email: "ruoliu.dev@gmail.com",
-    title: "Ruo Liu - SDE",
+    title: "Ruo Liu",
     description: "Ruo Liu's site",
 };
